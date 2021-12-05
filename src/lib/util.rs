326cf13e50f71d92297d8aa5692264a2a372524e
ccie18643 <ccie18643@gmail.com>
2021-12-05 01:16:15 +0000

    Addig the 'util' library containing various helper functions

---
+++ src/lib/util.rs
@@ -0,0 +1,42 @@
+/*
+############################################################################
+#                                                                          #
+#  RusTCP - Rust TCP/IP stack                                              #
+#  Copyright (C) 2020-2022  Sebastian Majewski                             #
+#                                                                          #
+#  This program is free software: you can redistribute it and/or modify    #
+#  it under the terms of the GNU General Public License as published by    #
+#  the Free Software Foundation, either version 3 of the License, or       #
+#  (at your option) any later version.                                     #
+#                                                                          #
+#  This program is distributed in the hope that it will be useful,         #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#  GNU General Public License for more details.                            #
+#                                                                          #
+#  You should have received a copy of the GNU General Public License       #
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.  #
+#                                                                          #
+#  Author's email: ccie18643@gmail.com                                     #
+#  Github repository: https://github.com/ccie18643/RusTCP                  #
+#                                                                          #
+############################################################################
+*/
+
+/// Create tracker string to be used as packet identifier and increment the serial number
+pub fn tracker(tag: &str, nic_name: &str, packet_sn: &mut usize) -> String {
+    let color = match tag {
+        "TX" => "lr",
+        "RX" => "lg",
+        _ => "/",
+    };
+    let tracker = format!(
+        "<{}>[{}/{}/{:04X}]</>",
+        color,
+        tag,
+        nic_name.to_uppercase(),
+        packet_sn
+    );
+    *packet_sn = (*packet_sn).wrapping_add(1);
+    tracker
+}
