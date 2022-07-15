157343a2cf91c0443d16653f17d9f00db54e9686
Sebastian Majewski <ccie18643@gmail.com>
2022-07-15 12:27:39 -0500

    Macros library

---
+++ src/lib/macros.rs
@@ -0,0 +1,50 @@
+/*
+############################################################################
+#                                                                          #
+#  RusTCP - Rust TCP/IP stack                                              #
+#  Copyright (C) 2020-2022  Sebastian Majewski                             #
+#                                                                          #
+#  This program is free software: you can redistribute it and/or modify    #
+#  it under the terms of the GNU General Public License as published by    #
+#  the Free Software Foundation, either version 3 of the License, or       #
+#  (at your option) any later version.                                     #
+#                                                                          #
+#  This program is distributed in the hope that it will be useful,         #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#  GNU General Public License for more details.                            #
+#                                                                          #
+#  You should have received a copy of the GNU General Public License       #
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.  #
+#                                                                          #
+#  Author's email: ccie18643@gmail.com                                     #
+#  Github repository: https://github.com/ccie18643/RusTCP                  #
+#                                                                          #
+############################################################################
+*/
+
+#![allow(dead_code)]
+
+#[macro_export]
+macro_rules! break_on_err {
+    ($res:expr) => {
+        match $res {
+            Ok(v) => v,
+            Err(_) => {
+                break;
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! continue_on_err {
+    ($res:expr) => {
+        match $res {
+            Ok(v) => v,
+            Err(_) => {
+                continue;
+            }
+        }
+    };
+}
